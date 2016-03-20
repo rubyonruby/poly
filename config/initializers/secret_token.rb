40e79858e29cc4342e4ebc9ff63e2d652aa1aefe
rubyonruby <appasani.n@gmail.com>
2016-03-20 18:27:34 -0400

    poly

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poly::Application.config.secret_key_base = '69fb6a04ba83a66b4a38eb9ec08e62e7351d9ccac66f318465cbb9dd1f5a4c2dc1b9067191a66309f1d8a5826df4190a765d73eccc8620b08c186e97d15ce06f'
